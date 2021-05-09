7be354447a5fcd7299e27bed72ad10af88f9ce79
Arben <arben.peposi@outlook.com>
2021-05-09 19:13:46 +0200

    backend admin , edit des propriétés

--- src/Controller/Admin/AdminPropertyController.php
+++ src/Controller/Admin/AdminPropertyController.php
@@ -1,20 +1,28 @@
 <?php
 namespace App\Controller\Admin;
 
+use App\Entity\Property;
+use App\Form\PropertyType;
 use App\Repository\PropertyRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdminPropertyController extends AbstractController 
 {
     
+    private $repository;
 
-    public function __construct(PropertyRepository $repository) {
+    public function __construct(PropertyRepository $repository, EntityManagerInterface $em) {
         $this->repository = $repository;
+        $this->em = $em;
     }
          
     /**
      * index
-     * @Route('/admin/admin.property.index)
+     * @Route("/admin", name="admin.property.index")
      * @return void
      */
     public function index() {
@@ -22,8 +30,20 @@ class AdminPropertyController extends AbstractController
         return $this->render("admin/property/index.html.twig", compact('properties'));
     }
 
-    public function edit(PropertyRepository $property) {
-        $properties = $this->repository->find($id);
-        return $this->render("admin/property/index.html.twig", compact('properties'));
+        
+    /**
+     * edit
+     * @Route("/admin/{id}", name="admin.property.edit")
+     * @param  mixed $property
+     * @return void
+     */
+    public function edit(Property $property, Request $request) {
+       $form = $this->createForm(PropertyType::class, $property);
+
+       
+        return $this->render("admin/property/edit.html.twig", [
+            'form' => $form->createView(),
+            'property' => $property,
+        ]);
     }
 }
